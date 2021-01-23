392623a29ca419c0c924ed2aa061d0cdf7829584
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-01-23 10:47:55 +0000

    Linux fixes

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ from setuptools import find_packages, setup
 setup(
     name='dnlp',
     author='Lord Alfred',
-    url='https://github.com/lord-alfred/dn
+    url='https://github.com/lord-alfred/dnlp',
+    license
