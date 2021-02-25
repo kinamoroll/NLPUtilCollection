407372db7533d2aeb5f3f2aaadc0164596aaae59
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-02-25 19:37:46 +0000

    Add material

--- src/dnlp/app.py
+++ src/dnlp/app.py
@@ -1,3 +1,15 @@
 
 import asyncio
 import logging
+
+from aiohttp import web
+
+from dnlp.helpers import sync
+from dnlp.routes import routes
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+)
+logger = logging.getLogger(__name__)
