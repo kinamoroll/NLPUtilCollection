d26efd868a0fd881b163557eef1a7609d85114e0
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-02-28 16:21:33 +0000

    working on UI

--- src/dnlp/app.py
+++ src/dnlp/app.py
@@ -13,3 +13,19 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
 )
 logger = logging.getLogger(__name__)
+
+
+async def get_app():
+    app = web.Application(client_max_size=1024 * 1024 * 25)  # 25MB
+    app.add_routes(routes)
+    return app
+
+
+@sync
+async def serve(host: str = '0.0.0.0', port: int = 9090):
+    runner = web.AppRunner(await get_app())
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+    logger.info(f'Listening on {host}:{port}')
+    await asyncio.Future()
