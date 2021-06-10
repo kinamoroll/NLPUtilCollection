1aa799e4fe2d96b094afe51f7089e2ae5fb48434
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-06-10 12:16:54 +0000

    Fixed typo in readme

--- src/dnlp/constants.py
+++ src/dnlp/constants.py
@@ -46,4 +46,5 @@ URL_REGEX = re.compile(
     r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"
     r"|"
     # host name
-    r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uf
+    r"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
+    
