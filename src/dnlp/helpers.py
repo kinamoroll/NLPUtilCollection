7d5d50ed0ee2c57fc5b19bff794898c039bf9460
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-08-30 19:05:04 +0000

    Added deployment script

--- src/dnlp/helpers.py
+++ src/dnlp/helpers.py
@@ -28,4 +28,4 @@ def deduplicate_sentences(sentences: List[str], threshold: float) -> List[str]:
     for i in range(len(sentences)):
         is_duplicated = False
 
-        for j
+        for j in ra
