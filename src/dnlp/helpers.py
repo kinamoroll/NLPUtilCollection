d2030fa34c59e6fd109c605e98e0ae7504f8783d
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-09-01 18:34:54 +0000

    Update method

--- src/dnlp/helpers.py
+++ src/dnlp/helpers.py
@@ -29,3 +29,4 @@ def deduplicate_sentences(sentences: List[str], threshold: float) -> List[str]:
         is_duplicated = False
 
         for j in range(i + 1, len(sentences)):
+            compared_ra
