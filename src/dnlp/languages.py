33ded4f5a73f7a5f59328d46156d9e8b995ae206
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-09-28 11:00:46 +0000

    Update fonts.css

---
+++ src/dnlp/languages.py
@@ -0,0 +1,17 @@
+
+# https://fasttext.cc/docs/en/language-identification.html
+# Language Code =>
+#   Language Family
+#   ISO language name
+#   Native name (endonym)
+#   ISO 639-1
+#   ISO 639-2/T
+#   ISO 639-2/B
+#   ISO 639-3
+FASTTEXT_LANGUAGES = {
+    'af': ('Indo-European', 'Afrikaans', 'Afrikaans', 'af', 'afr', 'afr', 'afr'),
+    'sq': ('Indo-European', 'Albanian', 'Shqip', 'sq', 'sqi', 'alb', 'sqi'),
+    'am': ('Afro-Asiatic', 'Amharic', 'አማርኛ', 'am', 'amh', 'amh', 'amh'),
+    'ar': ('Afro-Asiatic', 'Arabic', 'العربية', 'ar', 'ara', 'ara', 'ara'),
+    'an': ('Indo-European', 'Aragonese', 'aragonés', 'an', 'arg', 'arg', 'arg'),
+    'hy': ('Indo-European', 'Armenian', 'Հայերեն', 'hy', 'hye', 'arm', 'hye'),
