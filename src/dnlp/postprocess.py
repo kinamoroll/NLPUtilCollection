edf1f260a504756fa66439c027e27f52a54abafa
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-10-14 18:34:29 +0000

    correct readme

--- src/dnlp/postprocess.py
+++ src/dnlp/postprocess.py
@@ -9,3 +9,15 @@ def remap_prediction(prediction):
 
     for index in range(len(prediction[0])):
         lang = prediction[0][index].replace('__label__', '')
+
+        result.append({
+            'confidence': prediction[1][index],
+            'code': lang,
+            'name': FASTTEXT_LANGUAGES[lang][1],
+            'family': FASTTEXT_LANGUAGES[lang][0],
+            'endonym': FASTTEXT_LANGUAGES[lang][2],
+            'iso639-1': FASTTEXT_LANGUAGES[lang][3],
+            'iso639-2/T': FASTTEXT_LANGUAGES[lang][4],
+            'iso639-2/B': FASTTEXT_LANGUAGES[lang][5],
+            'iso639-3': FASTTEXT_LANGUAGES[lang][6],
+        })
