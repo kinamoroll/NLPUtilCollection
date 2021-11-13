42440267771472b6085e1c2b7cc59c6f38e14362
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-11-13 14:59:23 +0000

    Update CHANGES.txt

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -25,4 +25,5 @@ def preprocess_text(text: str) -> str:
 
     # custom `replace_with` values for more detectable results
     text = replace_urls(text, replace_with=' ')
-    text = replace_emails(text, replace_wi
+    text = replace_emails(text, replace_with=' ')
+  
