afe11a05946abb363c781b73b792a7041b7484ab
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-11-17 12:37:38 +0000

    new options

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -26,4 +26,6 @@ def preprocess_text(text: str) -> str:
     # custom `replace_with` values for more detectable results
     text = replace_urls(text, replace_with=' ')
     text = replace_emails(text, replace_with=' ')
-    text = replace_numbers(text, repla
+    text = replace_numbers(text, replace_with=' ')
+
+    te
