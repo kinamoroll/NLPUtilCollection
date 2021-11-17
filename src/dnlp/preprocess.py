4d972d479a6126021d3a6eff0f5083aaf6288e91
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2021-11-17 18:34:56 +0000

    changed readme.md

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -28,4 +28,5 @@ def preprocess_text(text: str) -> str:
     text = replace_emails(text, replace_with=' ')
     text = replace_numbers(text, replace_with=' ')
 
-    text = remove_p
+    text = remove_punct(text)
+   
