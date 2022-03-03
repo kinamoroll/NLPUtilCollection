be170488d7f2d2107dbd7a22dc692fcc40a8c6c6
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2022-03-03 20:43:38 +0000

    112

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -48,4 +48,4 @@ def fix_bad_unicode(text: str, normalization: str = 'NFC') -> str:
     Args:
         text (str): raw text
         normalization ({'NFC', 'NFKC', 'NFD', 'NFKD'}): if 'NFC',
-            combines characters and diacritics written
+            combines characters and diacritics written using 
