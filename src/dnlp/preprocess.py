8353b3714b4d7577814d2f8beb86032dc77d136e
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-02-16 08:33:52 +0000

    [INTERNAL][TEST] sap.uxap ObjectPageChild: reference images changed

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -70,4 +70,4 @@ def replace_urls(text: str, replace_with: str = '*URL*') -> str:
 
 def replace_emails(text: str, replace_with: str = '*EMAIL*') -> str:
     """Replace all emails in ``text`` str with ``replace_with`` str."""
-    return
+    return EMAIL_REGEX.
