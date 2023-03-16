3db9eb11d1284e821ae9b0e4f46ec1dc58478b1d
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-03-16 12:32:28 +0000

    Automated merge with ssh://jfxsrc.us.oracle.com//javafx/2.1/scrum/controls/jfx/rt

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -73,4 +73,5 @@ def replace_emails(text: str, replace_with: str = '*EMAIL*') -> str:
     return EMAIL_REGEX.sub(replace_with, text)
 
 
-def replace_numbers(text: str, replace_with: str = '*NUMBER*'
+def replace_numbers(text: str, replace_with: str = '*NUMBER*') -> str:
+    ""
