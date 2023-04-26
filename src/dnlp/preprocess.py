b5008d2fb13a084a1fc441501ff1d6d6ab440ccc
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-04-26 07:59:40 +0000

    Fix code style.

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -75,3 +75,4 @@ def replace_emails(text: str, replace_with: str = '*EMAIL*') -> str:
 
 def replace_numbers(text: str, replace_with: str = '*NUMBER*') -> str:
     """Replace all numbers in ``text`` str with ``replace_with`` str."""
+    return
