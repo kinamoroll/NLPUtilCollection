cab32de53feed2ca2f630db75289e972a5bf749b
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-06-29 07:25:46 +0000

    fix buttons

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -80,4 +80,4 @@ def replace_numbers(text: str, replace_with: str = '*NUMBER*') -> str:
 
 def remove_punct(text: str, marks: Optional[str] = None) -> str:
     """
-    Remove punc
+    Remove punctuation from ``t
