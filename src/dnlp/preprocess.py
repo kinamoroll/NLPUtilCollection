645b50b18db07c554f1e68635c6ee36a4f4616e1
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-07-27 21:34:36 +0000

    new ui

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -86,4 +86,4 @@ def remove_punct(text: str, marks: Optional[str] = None) -> str:
     Args:
         text (str): raw text
         marks (str): If specified, remove only the characters in this string,
-            e.g. ``ma
+            e.g. ``marks=',;:'``
