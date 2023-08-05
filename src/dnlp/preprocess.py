26ffbf861dfe0333a7df6bc809640df7316d49ea
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-08-05 16:16:36 +0000

    tweak test

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -87,4 +87,6 @@ def remove_punct(text: str, marks: Optional[str] = None) -> str:
         text (str): raw text
         marks (str): If specified, remove only the characters in this string,
             e.g. ``marks=',;:'`` removes commas, semi-colons, and colons.
-            Otherwise, all punctuation marks ar
+            Otherwise, all punctuation marks are removed.
+
+    
