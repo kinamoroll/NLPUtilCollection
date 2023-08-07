9e29176801eb52381c498b41648ae63b232e99ca
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-08-07 18:06:05 +0000

    remove unnecessary lines

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -95,4 +95,4 @@ def remove_punct(text: str, marks: Optional[str] = None) -> str:
     Note:
         When ``marks=None``, Python's built-in :meth:`str.translate()` is
         used to remove punctuation; otherwise, a regular expression is used
-        instead. The for
+        instead. The former's performa
