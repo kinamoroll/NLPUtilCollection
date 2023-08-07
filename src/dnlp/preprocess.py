f0bfcdbb944a1aeaa8321bbecce36ac4b11dc6b0
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-08-07 07:10:38 +0000

    gigablue: update bsp

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -94,4 +94,4 @@ def remove_punct(text: str, marks: Optional[str] = None) -> str:
 
     Note:
         When ``marks=None``, Python's built-in :meth:`str.translate()` is
-        used to remove punctuation; otherwise, 
+        used to remove punctuation; otherwise, a regular expression is 
