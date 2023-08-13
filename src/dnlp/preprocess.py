754d03ee3a268ba0ca61b296b238116ac03c4189
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-08-13 15:43:46 +0000

    fix validator

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -98,4 +98,4 @@ def remove_punct(text: str, marks: Optional[str] = None) -> str:
         instead. The former's performance is about 5-10x faster.
     """
     if marks:
-        return re.sub('[{}]+'.format(re.escape(marks)), ' 
+        return re.sub('[{}]+'.format(re.escape(marks)), ' ', text, fl
