ab3ca405731f13ae802ce25368b13b81071b4b10
kinamoroll <142219511+kinamoroll@users.noreply.github.com>
2023-09-07 06:29:35 +0000

    code formatter

--- src/dnlp/preprocess.py
+++ src/dnlp/preprocess.py
@@ -114,4 +114,4 @@ def normalize_whitespace(text: str) -> str:
 
 
 def normalize_html(html: str) -> str:
-    """Для чтобы слова из разных тегов не сл
+    """Для чтобы слова из разных тегов не сливались в о
